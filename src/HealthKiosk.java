85fe7a04a46e9b3f292e08ded33a95a11cc55971
Aba <kwofie.maba@gmail.com>
2025-09-27 21:09:53 +0000

    Lab2

---
+++ src/HealthKiosk.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+public class HealthKiosk {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Ashesi Health Kiosk!");
+
+        //task 1: service code
+        System.out.print("Enter service code (P/L/T/C): ");
+        String serviceCode = input.nextLine();
+
+        switch (serviceCode) {
+            case "P":
+            case "p":
+                System.out.println("Go to: Pharmacy Desk");
+                break;
+            case "L":
+            case "l":
+                System.out.println("Go to: Lab Desk");
+                break;
+            case "T":
+            case "t":
+                System.out.println("Go to: Triage Desk");
+                break;
+            case "C":
+            case "c":
+                System.out.println("Go to: Counselling Desk");
+                break;
+            default:
+                System.out.println("Invalid service code");
+                break;
+        }
+                //task 2: health metric
+
+            System.out.print("Kindly enter a health metric: ");
+            int metric = input.nextInt();
+
+            switch (metric) {
+                case 1:
+                    System.out.print("Kindly enter your weight (in kg): ");
+                    double weight = input.nextDouble();
+                    System.out.print("Kindly enter your height (in m): ");
+                    double height = input.nextDouble();
+                    double bmi = weight / (height * height);
+                    double bmiRounded = Math.round(bmi * 10) / 10.0;
+                    System.out.print("BMI: " + bmiRounded);
+                    if (bmiRounded < 18.5) {
+                        System.out.println("  Category: Underweight");
+                    } else if (bmiRounded >= 18.5 && bmiRounded <= 24.9) {
+                        System.out.println("  Category: Normal");
+                    } else if (bmiRounded >= 25 && bmiRounded <= 29.9) {
+                        System.out.println("  Category: Overweight");
+                    } else if (bmiRounded >= 30) {
+                        System.out.println("  Category: Obese");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Kindly enter your dosage (in mg): ");
+                    double dosage = input.nextDouble();
+                    double tabletDosage = 250.0;
+                    int tablets = (int) Math.ceil(dosage / tabletDosage);
+                    System.out.println("Number of tablets is " + tablets);
+                    break;
+                case 3:
+                    System.out.print("Kindly enter a angle (in degrees): ");
+                    double angle = input.nextDouble();
+                    double radians = Math.toRadians(angle);
+
+                    double sinValue = Math.sin(radians);
+                    double cosValue = Math.cos(radians);
+
+                    double sinRounded = Math.round(sinValue * 1000) / 1000.0;
+                    double cosRounded = Math.round(cosValue * 1000) / 1000.0;
+
+                    break;
+            }
+
+        }
+    }
+}
